e26451b9b498f573dcdf0878cd605126f591c637
Uriel Enrique <urielenrique99@gmail.com>
2022-08-13 12:07:20 -0500

    Initial Commit

---
+++ Pokedex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by Uriel Hernandez Gonzalez on 13/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Uriel Hernandez Gonzalez on 13/08/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

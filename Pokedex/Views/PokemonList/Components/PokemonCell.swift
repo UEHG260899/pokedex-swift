93f2f6f55fb9c47d7a69a0f5bcf80f21f7b4f33c
UEHG260899 <67031721+UEHG260899@users.noreply.github.com>
2022-08-15 18:16:03 -0500

    Pokemon grid view (#2)
    * Basic grid layout done
    
    * Adding pokemon type colors and pokemonCell
    
    * Finished PokemonCell

---
+++ Pokedex/Views/PokemonList/Components/PokemonCell.swift
@@ -0,0 +1,42 @@
+//
+//  PokeminCell.swift
+//  Pokedex
+//
+//  Created by Uriel Hernandez Gonzalez on 14/08/22.
+//
+
+import SwiftUI
+
+struct PokemonCell: View {
+    
+    let width: CGFloat
+    let pokemonTypeColor: AppConfigurations.PokemonTypeColors
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                Color(pokemonTypeColor.rawValue)
+                    .frame(width: width, height: 150)
+                Image("Bulbasaur")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+            }
+            
+            Text("Bulbasaur")
+                .customFont(.titilliumRegular, size: 17, relativeTo: .body)
+                .lineLimit(2)
+                .minimumScaleFactor(0.5)
+                .padding()
+        }
+        .background(Color.white)
+        .cornerRadius(16)
+        .shadow(color: Color("Accent"),radius: 10, x: 5, y: 5)
+    }
+}
+
+struct PokeminCell_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonCell(width: 200, pokemonTypeColor: .plantType)
+            .previewLayout(.sizeThatFits)
+    }
+}
---
+++ Pokedex/Views/PokemonList/PokemonGrid.swift
@@ -0,0 +1,38 @@
+//
+//  PokemonGrid.swift
+//  Pokedex
+//
+//  Created by Uriel Hernandez Gonzalez on 13/08/22.
+//
+
+import SwiftUI
+
+struct PokemonGrid: View {
+    
+    var pokemonColumns: [GridItem] {
+        [GridItem(.flexible(minimum: 150, maximum: .infinity), spacing: 20),
+         GridItem(.flexible(minimum: 150, maximum: .infinity), spacing: 20)]
+    }
+    
+    var body: some View {
+        GeometryReader { proxy in
+            ScrollView {
+                LazyVGrid(columns: pokemonColumns, spacing: 20) {
+                    ForEach(1...10, id: \.self) { _ in
+                        NavigationLink(destination: Text("Hello, Pokemon")) {
+                            PokemonCell(width: proxy.size.width / 2 - 20,
+                                        pokemonTypeColor: .plantType)
+                        }
+                    }
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+struct PokemonGrid_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonGrid()
+    }
+}
